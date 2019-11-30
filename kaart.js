d1173ba81de910d413450cc7d07abf5c5e27601d
Kristo Markov <markovkristo@gmail.com>
2019-11-30 16:24:39 +0200

    Pisi muudatused

--- kaart.js
+++ kaart.js
@@ -1,10 +1,10 @@
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
+  // The location of Rome
+  var Rome = {lat: 41.934197, lng: 12.454682};
+  // The map, centered at Rome
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+      document.getElementById('map'), {zoom: 4, center: Rome});
+  // The marker, positioned at Rome
+  var marker = new google.maps.Marker({position: Rome, map: map});
 }
